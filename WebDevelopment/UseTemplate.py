46b9e42256e2db27f04e348caeda9a3bc3b09ff2
sfy394953843 <394953843@qq.com>
2018-07-19 13:41:21 +0800

    web 开发和异步 IO

--- WebDevelopment/UseTemplate.py
+++ WebDevelopment/UseTemplate.py
@@ -121,13 +121,40 @@ mvc-form
 
 除了Jinja2，常见的模板还有：
 
-Mako：用<% ... %>和${xxx}的一个模板；
+Mako：用<% ... %>和${xxx}的一个模板； http://www.makotemplates.org/
 
-Cheetah：也是用<% ... %>和${xxx}的一个模板；
+Cheetah：也是用<% ... %>和${xxx}的一个模板；http://cheetahtemplate.org/
 
-Django：Django是一站式框架，内置一个用{% ... %}和{{ xxx }}的模板。
+Django：Django是一站式框架，内置一个用{% ... %}和{{ xxx }}的模板。https://www.djangoproject.com/
 
 小结
 有了MVC，我们就分离了Python代码和HTML代码。HTML代码全部放到模板里，写起来更有效率。
 
 """
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return render_template('form.html')
+
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'admin' and password == 'password':
+        return render_template('signin-ok.html', username=username)
+    return render_template('form.html', message='Bad username or password', username=username)
+
+
+if __name__ == '__main__':
+    app.run()
